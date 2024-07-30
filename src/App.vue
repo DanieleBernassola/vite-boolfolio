cbfd394e8bd1331173a979a991038ff67f7dae78
DanieleBernassola <danielebernassola02@gmail.com>
2024-07-30 17:03:10 +0200

    [UPD] Installed fontawesome

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
   <div class="container">
     <h1>Vue Vite Template</h1>
     <p>Template per progetti Vue con Vite</p>
+    <font-awesome-icon :icon="['fas', 'user-secret']" />
   </div>
 </template>
 <style></style>
